27d86d1c9b14adf9da151fa2a90f329a11d8280b
Alex Juneo Modesto Damasceno <alex1@cefetmg.br>
2020-02-28 19:08:12 -0300

    Problema URI 1051 solucionado

--- uri_1051/Main.java
+++ uri_1051/Main.java
@@ -10,24 +10,20 @@ public class Main {
 
 		double salario;
 		salario = sc.nextDouble();
-
 		double calculo_irpf_8, calculo_irpf_18, calculo_irpf_28;
 		double verificar_salario = salario; 	
 		double total_pagar_imposto = 0;
 
-		DecimalFormat df = new DecimalFormat("####.###");   
-
-		if(salario <= 2000.00){
-			System.out.println("Isento");
-		}   
-		else if((verificar_salario >= 2000.01) && (verificar_salario <= 3000.00)){ //caso de teste 2500 ---- total a pagar: 40.00 // caso de teste 3000 ---- total a pagar: 80.00
+		DecimalFormat df = new DecimalFormat("###0.00");
+		
+		if((verificar_salario >= 2000.01) && (verificar_salario <= 3000.00)){
 			salario = (salario - 2000);
 			calculo_irpf_8 = (salario * 8) / 100;
-			if(salario <= 1000){	
+			if(salario <= 1000){
 				total_pagar_imposto = calculo_irpf_8;
 			}
 		}
-		else if((verificar_salario >= 3000.01) && (verificar_salario <= 4500.00)){ //caso de teste 3500 --- total a pagar: 170.00 // caso de teste 4500 ---- total a pagar: 350.00
+		else if((verificar_salario >= 3000.01) && (verificar_salario <= 4500.00)){
 			salario = (salario - 2000);
 			calculo_irpf_8 = (1000 * 8) / 100;
 			salario = (salario - 1000);
@@ -48,13 +44,17 @@ public class Main {
 					calculo_irpf_18 = (1500 * 18) / 100;
 					salario = (salario - 1500.00);
 					if(salario > 0.0){
-						calculo_irpf_28 = (salario * 28) / 100; //aplicou o resto do salário nos 28%
-						salario = salario - salario; //zerou o salário
-						total_pagar_imposto = (calculo_irpf_8) + (calculo_irpf_18) + (calculo_irpf_28); //pegou os valores do imposto de 8, 18 e 28 e somou o total na variável total_pagar_imposto	
+						calculo_irpf_28 = (salario * 28) / 100;
+						total_pagar_imposto = (calculo_irpf_8) + (calculo_irpf_18) + (calculo_irpf_28); 
 					}
 				}
 			}
-		}  
-		System.out.println("R$ " + df.format(total_pagar_imposto));
+		} 
+		if(total_pagar_imposto >= 0.01){
+			System.out.println("R$ " + df.format(total_pagar_imposto));
+		}
+		else{
+			System.out.println("Isento");
+		}
     }
 }
