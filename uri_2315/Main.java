d8f6f34e6f16f96888ff765f0624f06153adc7ef
Alex Juneo Modesto Damasceno <alex1@cefetmg.br>
2020-03-17 16:37:28 -0300

    Atualizações de código no URI 2315

--- uri_2315/Main.java
+++ uri_2315/Main.java
@@ -13,18 +13,18 @@ import java.io.IOException;
  * SAÍDA:
  * A saída deverá conter o número de dias entre a primeira e a segunda data.
  * 
- * jan - 31
- * fev - 28
- * mar - 30
- * abr - 30
- * mai - 31
- * jun - 29
- * jul - 31
- * ago - 31
- * set - 30
- * out - 31
- * nov - 30
- * dez - 31
+ * jan = 31
+ * fev = 28
+ * mar = 30
+ * abr = 30
+ * mai = 31
+ * jun = 29
+ * jul = 31
+ * ago = 31
+ * set = 30
+ * out = 31
+ * nov = 30
+ * dez = 31
  * 
  */
 
@@ -42,8 +42,12 @@ public class Main{
         dia_final = sc.nextInt();
         mes_final = sc.nextInt();
         int total_dia = 0;
+        int total_dia_prox_mes = 0;
+        int totalzao = 0;
+        int mes = 0;
+        int subtracao_mes = 0;
 
-        //caso em 1 mês só.
+
         if(mes_inicial == mes_final){
             for(int i = dia_inicial; i < dia_final; i++){
                 total_dia = total_dia + 1;
@@ -51,16 +55,58 @@ public class Main{
             System.out.println(total_dia);
         }
 
-        //caso mais de 1 mês
+
+        /**
+         * 
+         * soma a quantidade de dias até o final do mes correspondente
+         * subtracao mes final com o inicial
+         * se a subtracao der mais que 1, pegar o mes seguinte somar o total dia dele
+         * somar os proximos meses até o ultimo mes
+         * no ultimo mes somar a quantidade de dias
+         * 
+         */
+
+
         if(mes_inicial < mes_final){
-            for(int i = dia_inicial; i < 31; i++){ //percorrer o for até o dia 31.
+            mes = quantidadeDiasMes(mes_inicial);
+            for(int i = dia_inicial; i <= mes; i++){ 
                 total_dia = total_dia + 1;
-                for(int l = 0; l < dia_final; l++){
-                    total_dia += 1;
-                }
-            }    
-            System.out.println(totale);
+            }  
+            for(){
+                
+            }
+            for(int l = 1; l <= dia_final; l++){
+                total_dia_prox_mes = total_dia_prox_mes + 1;
+            } 
+            totalzao = total_dia + total_dia_prox_mes;
+            System.out.println(totalzao);
         }
+
+
+
         sc.close();
+    }
+
+    public static int quantidadeDiasMes(int mes_inicial){
+
+        int mes = 0;
+
+        if(mes_inicial == 1 || mes_inicial == 5 || mes_inicial == 7 || mes_inicial == 8 || mes_inicial == 10 || mes_inicial == 12){
+            mes = 31;
+        }
+        else if(mes_inicial == 3 || mes_inicial == 4 || mes_inicial == 8 || mes_inicial == 11){
+            mes = 30;
+        }
+        else if(mes_inicial == 6){
+            mes = 29;
+        }
+        else{
+            mes = 28;
+        }
+        
+        if(mes != 0){
+            return mes;
+        }
+        return 0;
     }
 }
